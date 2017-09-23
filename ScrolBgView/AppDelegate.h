7c2340947ddb7045d9eeeb3b83f7dcc2500ae0be
HanMJ <915660592@qq.com>
2017-09-23 15:17:40 +0800

    Initial Commit

---
+++ ScrolBgView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrolBgView
+//
+//  Created by MJ on 2017/9/23.
+//  Copyright © 2017年 韩明静. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
